e304c5e570afc7c2a64f06faf7247a751231d425
rodrigodoprado <rodrigodoprado1992@gmail.com>
2023-03-12 20:40:41 -0300

    fix: componet/navbar componet/card

---
+++ src/componet/footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <>
+      {/* <!-- Footer--> */}
+      <footer className="py-5 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
+            Copyright &copy; Your Website 2022
+          </p>
+        </div>
+      </footer>
+    </>
+  )
+}
